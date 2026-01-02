aab4ac53d3922b561876b08b082044fa1accfe8e
ranja7488-svg <ranja7488@gmail.com>
2026-01-02 23:00:06 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,19 @@
+const mysql = require("mysql2");
+
+const db = mysql.createConnection({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: 3306
+});
+
+db.connect(err => {
+  if (err) {
+    console.log("DB Error", err);
+  } else {
+    console.log("MySQL Connected");
+  }
+});
+
+module.exports = db;
