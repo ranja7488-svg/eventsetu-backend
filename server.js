d870d3bc26d9481d2d326c5006bd566351bc17fd
ranja7488-svg <ranja7488@gmail.com>
2026-01-03 00:52:51 +0530

    Implement CORS support in server.js
    Added CORS middleware to allow cross-origin requests.

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 
+// 🔥 CORS allow (important)
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("EventSetu Backend Running");
 });
