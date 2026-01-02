766cbab94573ac9aabcbb4bc1cf2fd91882d2b63
ranja7488-svg <ranja7488@gmail.com>
2026-01-02 23:43:08 +0530

    Add files via upload

--- db.js
+++ db.js
@@ -8,9 +8,9 @@ const db = mysql.createConnection({
   port: 3306
 });
 
-db.connect((err) => {
+db.connect(err => {
   if (err) {
-    console.log("DB Connection Error:", err.message);
+    console.log("DB Error", err);
   } else {
     console.log("MySQL Connected");
   }
